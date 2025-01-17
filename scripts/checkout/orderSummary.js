c30fd57591aadf30f87afdbbb513a1b142db520d
MarcselloDev <88592581+MarcselloDev@users.noreply.github.com>
2025-01-17 21:57:43 +0100

    Update delivery dates to not include weekends

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -2,7 +2,7 @@ import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeli
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js"
-import { deliveryOptions, getDeliveryOptions } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOptions, calculateDeliveryDate } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary () {
@@ -19,9 +19,7 @@ export function renderOrderSummary () {
 
     let deliveryOption = getDeliveryOptions(deliveryOptionId)
 
-    const today = dayjs()
-    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days')
-    const dateString = deliveryDate.format('dddd, MMMM D')
+    const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML +=`
     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -71,9 +69,9 @@ export function renderOrderSummary () {
     let html = ''
 
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs()
-      const deliveryDate = today.add(deliveryOption.deliveryDays, 'days')
-      const dateString = deliveryDate.format('dddd, MMMM D')
+
+      const dateString = calculateDeliveryDate(deliveryOption);
+      
       const priceString = deliveryOption.priceCents === 0 ?  'FREE' : `$${formatCurrency(deliveryOption.priceCents)}`
 
       const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
