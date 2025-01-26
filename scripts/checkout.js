cef4c0ecab90acfd3a6d36fa0c7f82a24e7ef98e
MarcselloDev <88592581+MarcselloDev@users.noreply.github.com>
2025-01-26 15:53:00 +0100

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,13 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import "../data/car.js"
-import "../data/backend-practice.js"
+import { loadProducts } from "../data/products.js";
+// import "../data/backend-practice.js"
 // import '../data/cart-oop.js'
 // import '../data/cart-class.js'
+loadProducts(() => {
+  renderOrderSummary()
+  renderPaymentSummary()
+})
 
-renderOrderSummary()
-renderPaymentSummary()
+  
